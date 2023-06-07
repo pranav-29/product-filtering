131aea52454e9c6ce4b39ae0a1ac9bf24c382044
pranav-29 <pranavkumar679784@gmail.com>
2023-06-08 01:40:44 +0530

    All Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { products, categories } from './data/data';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css'; 
 
-function App() {
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  const [animatedText, setAnimatedText] = useState('');
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+    setFilteredProducts(
+      category === ''
+        ? products
+        : products.filter((product) => product.category === category)
+    );
+  };
+
+  const handleBuyNow = (product) => {
+    // Handle the buy now functionality for the product
+    console.log('Buy Now:', product);
+  };
+
+  useEffect(() => {
+    const textArray = ['Missan', 'Product', 'Filter', 'Assignment', 'Submitted By','Pranav Choudhary'];
+    let currentIndex = 0;
+
+    const intervalId = setInterval(() => {
+      setAnimatedText(textArray[currentIndex]);
+      currentIndex = (currentIndex + 1) % textArray.length;
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1 className="animated-text">{animatedText}</h1>
       </header>
+
+      <main>
+        <h2 className="headings">Categories</h2>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle btn-category"
+            type="button"
+            id="categoryDropdown"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {selectedCategory === '' ? 'All Categories' : selectedCategory}
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="categoryDropdown">
+            <li>
+              <button
+                className="dropdown-item"
+                onClick={() => handleCategorySelect('')}
+              >
+                All Categories
+              </button>
+            </li>
+            {categories.map((category) => (
+              <li key={category}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => handleCategorySelect(category)}
+                >
+                  {category}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <h2 className='headings'>Products</h2>
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+          {filteredProducts.map((product) => (
+            <div className="col" key={product.id}>
+              <div className="card product-card">
+                <div className="product-img-wrapper">
+                  <img
+                    src={product.imageURL}
+                    className="card-img-top"
+                    alt={product.name}
+                  />
+                </div>
+                <div className="card-body">
+                  <h6 className="card-title">{product.name}</h6>
+                  <h3 className="card-text">Price: {product.price}</h3>
+                  <button
+                    className="btn btn-buy-now" onClick={() => handleBuyNow(product)}
+                  >
+                    Buy Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      <footer>
+        {/* Footer content */}
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,75 @@
+export const products = [
+    {
+        id: 1,
+        category: 'Laptop',
+        name: 'Samsung-Notebook-9-pro',
+        imageURL: 'https://d11zer3aoz69xt.cloudfront.net/media/catalog/product/cache/1/image/1200x/040ec09b1e35df139433887a97daa66f/s/a/samsung_notebook_9_pro_156_core_i7_6th_gen_256gb_8gb_ram_multi-touch_laptop_4.jpg',
+        price: '49,999'
+    },
+    {
+        id: 2,
+        category: 'Laptop',
+        name: 'Lenovo_80r4000vus',
+        imageURL: 'https://www.bhphotovideo.com/images/images2500x2500/lenovo_80r4000vus_15_6_flex_3_multi_touch_1219643.jpg',
+        price: '61,999'
+    },
+    {
+        id: 3,
+        category: 'Smart Phone',
+        name: 'Samsung Galaxy S21 FE',
+        imageURL: 'https://th.bing.com/th/id/OIP.gJIrZiUa84DnhglEjUr6BQHaGQ?pid=ImgDet&w=1024&h=865&rs=1',
+        price: '52,999'
+    },
+    {
+        id: 4,
+        category: 'Smart Phone',
+        name: 'Motorola G60 5G',
+        imageURL: 'https://brmotorolanew.vtexassets.com/arquivos/ids/158173/Smartphone-Moto-G60-128-GB-Imagem-Frontal-Curvada-Azul.png?v=637552007893870000',
+        price: '25,999'
+    },
+    {
+        id: 5,
+        category: 'Laptop',
+        name: 'Asus 14.0 Laptop',
+        imageURL: 'https://th.bing.com/th/id/OIP.VK11JfTnWbDN6KZ-DwE0jgHaE5?pid=ImgDet&rs=1',
+        price: '54,999'
+    },
+    {
+        id: 6,
+        category: 'Ear Phones',
+        name: 'MI True Wireless 2 Pro',
+        imageURL: 'https://th.bing.com/th/id/OIP._sNqv1NcKw7jGrlSEXcZFAHaHF?pid=ImgDet&rs=1',
+        price: '2,299'
+    },
+    {
+        id: 7,
+        category: 'Ear Phones',
+        name: 'Sony Overear Headphone Wierd',
+        imageURL: 'https://n2.sdlcdn.com/imgs/b/g/m/Sony-MDR-XB950AP-Extra-Bass-SDL122806821-2-eb5aa.jpg',
+        price: "5,999"
+    },
+    {
+        id: 8,
+        category: 'Ear Phones',
+        name: 'Boat Nirvana Ion',
+        imageURL: 'https://badisoch.in/wp-content/uploads/2023/03/E7VOgOjb.jpg',
+        price: "1,999"
+    },
+    {
+        id: 9,
+        category: 'Laptop',
+        name: 'HP Hewlett Packard',
+        imageURL: 'https://www.bhphotovideo.com/images/images2500x2500/HP_Hewlett_Packard_WQ809UA_ABA_Mini_110_3030NR_10_1_Netbook_703253.jpg',
+        price: "39,999"
+    },
+    {
+        id: 10,
+        category: 'Smart Phone',
+        name: 'Nokia 3.2',
+        imageURL: 'https://static.digit.in/product/6a3dd5ecc3145f48e9fd391ff86aec33f607ee8d.jpeg?tr=w-1200',
+        price: "5,999"
+    }
+
+];
+
+export const categories = ['Smart Phone', 'Laptop', 'Ear Phones'];
